f610b5b7630eb2b2db18aa2e22ee05c44642210f
Satoshi Shiraishi <sachoco@gmail.com>
2015-02-27 18:27:19 +0000

    Add linebreak

---
+++ tinymce/br.js
@@ -0,0 +1,11 @@
+(function() {
+    tinymce.PluginManager.add('br_button', function( editor, url ) {
+        editor.addButton( 'br_button', {
+            text: 'LineBreak',
+            icon: false,
+            onclick: function() {
+                editor.insertContent('<br class="none" /> ');
+            }
+        });
+    });
+})();
