4ca372c6efeabb6f9e0e527e44fb79726a1314e4
Satoshi Shiraishi <sachoco@gmail.com>
2015-02-27 18:24:23 +0000

    add linebreak in the editor

--- functions.php
+++ functions.php
@@ -397,4 +397,29 @@ function languages_list(){
     }
 }
 
+add_action('admin_head', 'add_br_button');
+function add_br_button() {
+    global $typenow;
+    // check user permissions
+    if ( !current_user_can('edit_posts') && !current_user_can('edit_pages') ) {
+    return;
+    }
+    // verify the post type
+    if( ! in_array( $typenow, array( 'post', 'page' ) ) )
+        return;
+    // check if WYSIWYG is enabled
+    if ( get_user_option('rich_editing') == 'true') {
+        add_filter("mce_external_plugins", "add_br_plugin");
+        add_filter('mce_buttons', 'register_br_button');
+    }
+}
+function add_br_plugin($plugin_array) {
+    $plugin_array['br_button'] = get_template_directory_uri() . '/tinymce/br.js';
+    return $plugin_array;
+}
+function register_br_button($buttons) {
+   array_push($buttons, "br_button");
+   return $buttons;
+}
+
 ?>
